1076f12d8b1c3de202e822f4a07b4e21386a6690
claes <claesca@outlook.com>
2017-05-13 13:00:30 +0200

    add form with drop down box and route for the form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root controller: :restaurants, action: :index
   resources :restaurants, only: [:show]
+
+  get 'rest_cat', controller: :restaurants, action: :rest_cat, as: :rest_cat
+  
 end
