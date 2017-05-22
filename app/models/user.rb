579c95a9b05d47b3654e589606ee8c489df5f824
claes <claesca@outlook.com>
2017-05-22 11:36:11 +0200

    add rspec test, factorygirl and presence validaitons

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :username, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
 end
