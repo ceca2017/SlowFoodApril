c21ed3a7a345d7c29d0cf2605dfc3214331546f0
claes <claesca@outlook.com>
2017-06-03 16:31:28 +0200

    minor updates to charge controller and the new and application views

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,13 +1,14 @@
 class ChargesController < ApplicationController
   def new
     binding.pry
-  end
-
-  def create
     @items = session[:cart]
     @total_price = 0
     @items.each do |item|
       @total_price += item['price']
     end
+  end
+
+  def create
+
   end
 end
