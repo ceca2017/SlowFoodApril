ca6422b1a651f6f0ca6fc84fa6769e97d4a28814
claes <claesca@outlook.com>
2017-06-03 18:57:48 +0200

    minor update to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-
-
 users= User.create([{ email: 'owner@b.com',
                       password: '12345678',
                       password_confirmation: '12345678',
